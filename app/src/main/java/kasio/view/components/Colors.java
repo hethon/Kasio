9c5bd0c9ad9a0234ca21c03f6cddd789c4ef0f01
Henok <henoktesfamikael16@gmail.com>
2026-03-29 16:15:34 +0300

    feat(ui): update calculator display color

--- app/src/main/java/kasio/view/components/Colors.java
+++ app/src/main/java/kasio/view/components/Colors.java
@@ -7,7 +7,7 @@ public class Colors {
   public static final Color SECONDARY = new Color(0x535353);
   public static final Color RED = new Color(0xa64246);
   public static final Color GREEN = new Color(0x27954e);
-  public static final Color DISPLAY = new Color(0x7a8542);
+  public static final Color DISPLAY = new Color(0x939c96);
   public static final Color WHITE = Color.WHITE;
   public static final Color BLACK = PRIMARY;
 }
